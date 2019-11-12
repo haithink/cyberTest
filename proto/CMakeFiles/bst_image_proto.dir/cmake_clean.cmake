bb242138799909eaa26cd1b9db40034bebf7408a
haiyan.pang <haiyan.pang@bst.ai>
2019-11-12 15:35:57 +0800

    init

---
+++ proto/CMakeFiles/bst_image_proto.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "config.pb.cc"
+  "config.pb.h"
+  "CMakeFiles/bst_image_proto.dir/config.pb.cc.o"
+  "libbst_image_proto.pdb"
+  "libbst_image_proto.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bst_image_proto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
